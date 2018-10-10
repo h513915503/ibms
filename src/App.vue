bd2bbbb6a99bfdddab1c55fa02469abdcba186d1
hanyang <513915503@qq.com>
2018-10-10 11:52:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+	<div id="app">
+		<el-header>Header</el-header>
+
+		<el-container>
+			<el-aside width="200px">Aside</el-aside>
+
+			<el-main>
+				<router-view/>
+			</el-main>
+		</el-container>
+	</div>
+</template>
