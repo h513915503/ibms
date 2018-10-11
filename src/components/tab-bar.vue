521f659f44ab8fb2007593fb381097b0a96aa82e
hanyang <513915503@qq.com>
2018-10-11 17:19:29 +0800

    添加组件文件夹

---
+++ src/components/tab-bar.vue
@@ -0,0 +1,42 @@
+<style scoped>
+.tabs {
+	display: flex;
+	align-items: center;
+	padding: 12px 0;
+	margin: 24px;
+	color: #000;
+	font-size: 16px;
+	background-color: #FFF;
+}
+.tabs-item {
+	margin: 0 30px;
+	text-align: center;
+
+	& i {
+		display: block;
+		margin-bottom: 15px;
+		font-size: 24px;
+	}
+}
+</style>
+
+<template>
+	<div class="tabs">
+		<div class="tabs-item" v-for="item of list">
+			<i v-text="item.number"></i>
+			<span v-text="item.text"></span>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['list'],
+
+		data() {
+			return {
+
+			}
+		}
+	}
+</script>
