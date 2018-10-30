d3950b2b50da1527ccd7260f1c4982b7afdff6a7
hanyang <513915503@qq.com>
2018-10-30 15:10:50 +0800

    opt: 1. 添加退租和编辑逻辑

--- src/views/parking-lot/Detail.vue
+++ src/views/parking-lot/Detail.vue
@@ -1,28 +1,41 @@
 <style scoped>
 #parking-lot-detail-wrapper {
 	padding: 24px;
+	position: relative;
+
+	& .popover-wrapper {
+		right: 48px;
+		top: 140px;
+	}
+
+	& .throw-lease-wrapper {
+		right: 240px;
+		top: 140px;
+	}
 }
 .container {
 	padding: 24px;
 	margin-top: 20px;
 	box-sizing: border-box;
 	background-color: #FFF;
+
+	& .btn-relt {
+		margin-left: auto;
+		margin-right: 20px;
+	}
+
+	& .btn-edit {
+		margin-left: 0;
+		margin-right: 20px;
+	}
 }
 .btn-wrapper {
 	display: flex;
 	margin-bottom: 32px;
 }
-#parking-lot-detail-wrapper .btn-relt {
-	margin-left: auto;
-	margin-right: 20px;
-}
-#parking-lot-detail-wrapper .btn-edit {
-	margin-left: 0;
-	margin-right: 20px;
-}
 .btn-throw-lease {
 	color: red;
-	background-color: #EEE;
+	background-color: rgba(0, 0, 0, .04);
 }
 .item {
 	margin-bottom: 40px;
@@ -61,69 +74,111 @@ strong {
 			<el-breadcrumb-item>包月车辆详情</el-breadcrumb-item>
 		</el-breadcrumb>
 
-		<div class="container">
+		<loading v-if="loading"></loading>
+		<div class="container" v-else>
 			<div class="btn-wrapper">
 				<el-button @click="back">返回</el-button>
-				<el-button class="btn-relt">续租</el-button>
-				<el-button class="btn-edit">编辑</el-button>
 
-				  <el-popover placement="bottom-end" title="该车主确定退租车位吗？" width="400" trigger="click" content="公司退租后，该公司在园区的信息将被删除。">
-    					<el-button slot="reference" class="btn-throw-lease">退租</el-button>
+				<el-button class="btn-relt" @click="throwLeaseModalStatus = true">续租</el-button>
+				<el-button class="btn-edit" @click="index = 1">编辑</el-button>
+				<el-button slot="reference" class="btn-throw-lease" @click="popoverModalStatus = true">退租</el-button>
 
-    					<p class="popover-content">车主退租后，该车主在园区的信息将被删除。</p>
-    					<div class="popover-btn-wrapper">
-    						<el-button size="small">取消</el-button>
-							<el-button size="small" class="btn-throw-lease">确定</el-button>
-    					</div>
-  				</el-popover>
+				<popover name="close" title="该车主确定退租车位吗？" content="公司退租后，该公司在园区的信息将被删除。" :popoverModalStatus.sync="popoverModalStatus">
+					<el-button slot="ok" @click="popoverModalStatus = false">取消</el-button>
+					<el-button type="primary" slot="cancel" class="ok" @click="throwLease">确定</el-button>
+				</popover>
 			</div>
 
-			<div class="detail-content">
+			<parking-lot-add :type="1" @cancel="cancel" @ok="editComplete" v-if="index === 1"></parking-lot-add>
+
+			<div class="detail-content" v-else>
 				<div class="item">
 					<span>车牌号：</span>
-					<strong v-text="carNumber"></strong>
+					<strong v-text="detail.carNumber"></strong>
 				</div>
 				<div class="item">
 					<span>车主姓名：</span>
-					<strong>{{name}}</strong>
+					<strong>{{detail.name}}</strong>
 				</div>
 				<div class="item">
 					<span>车主号码：</span>
-					<strong>{{phone}}</strong>
+					<strong>{{detail.phone}}</strong>
 				</div>
 				<div class="item">
 					<span>所在单位：</span>
-					<strong v-text="company"></strong>
+					<strong v-text="detail.company"></strong>
 				</div>
 				<div class="item">
 					<span>到期日期：</span>
-					<strong v-text="date"></strong>
+					<strong v-text="detail.date"></strong>
 				</div>
 				<div class="item">
 					<span>车位：</span>
-					<strong v-text="parkingPlace"></strong>
+					<strong v-text="detail.parkingPlace"></strong>
 				</div>
 			</div>
 		</div>
+
+		<parking-lot-throw-lease :throwLeaseModalStatus.sync="throwLeaseModalStatus"></parking-lot-throw-lease>
 	</div>
 </template>
 
 <script>
+	import getResponses from '@/api'
+	import parkingLotAdd from '@/components/parking-lot/Add'
+	import parkingLotThrowLease from '@/components/parking-lot/throw-lease'
+
 	export default {
 		data() {
 			return {
-				carNumber: '浙Aml1223',
-				name: '大苏打',
-				phone: 18600003333,
-				company: '大飒飒大飒飒',
-				date: '2018-12-12',
-				parkingPlace: '-1层'
+				loading: false,
+				popoverModalStatus: false,
+				throwLeaseModalStatus: false,
+
+				index: 0,
+
+				detail: {}
 			}
 		},
 
+		components: {
+			parkingLotAdd,
+			parkingLotThrowLease
+		},
+
+		created() {
+			this.getDetail()
+		},
+
 		methods: {
+			async getDetail() {
+				const params = {
+					id: this.$route.params.id
+				}
+
+				this.loading = true
+
+				const data = await getResponses('/api/getDetail', params)
+
+				this.loading = false
+
+				if (! data) {
+					return
+				}
+
+				this.detail = data.data
+			},
 			back() {
-				this.$router.replace('/lease')
+				this.$router.push('/lease')
+			},
+			cancel() {
+				this.index = 0
+			},
+			editComplete() {
+
+			},
+			throwLease() {
+
 			}
 		}
 	}
