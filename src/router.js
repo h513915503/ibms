7a0552bb9bc5b7c975711897066bad1d86a1f8b8
hanyang <513915503@qq.com>
2018-11-08 19:16:48 +0800

    update: 实现权限控制

--- src/router.js
+++ src/router.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from './store'
+
+Vue.use(Router)
 
 import Overview from './views/overview/Index.vue'
 import Lease from './views/lease/Index.vue'
@@ -39,123 +42,13 @@ import Dashboard from './views/Dashboard/Dashboard.vue'
 
 import Environment from './views/environment/Index.vue'
 
-Vue.use(Router)
+import NotFound from './views/not-found/index.vue'
+
 
 const router = new Router({
 	mode: 'history',
 	base: process.env.BASE_URL,
 	routes: [
-		{
-			path: '/',
-			name: 'overview',
-			component: Overview
-		},
-
-		{
-			path: '/lease',
-			name: 'lease',
-			component: Lease
-		},
-		{
-			path: '/lease/add',
-			name: 'lease-add',
-			component: LeaseAdd
-		},
-		{
-			path: '/lease/detail/:id',
-			name: 'lease-detail',
-			component: LeaseDetail
-		},
-
-		{
-			path: '/parking-lot',
-			name: 'parking-lot',
-			component: ParkingLot
-		},
-		{
-			path: '/parking-lot/add',
-			name: 'parking-lot-add',
-			component: ParkingLotAdd
-		},
-		{
-			path: '/parking-lot/detail/:id',
-			name: 'parking-lot-detail',
-			component: ParkingLotDetail
-		},
-
-		{
-			path: '/proprietor',
-			name: 'proprietor',
-			component: Proprietor
-		},
-		{
-			path: '/lightManagement',
-			name: 'lightManagement',
-			component: LightManagement
-		},
-		{
-			path: '/visitor',
-			name: 'visitor',
-			component: Visitor
-		},
-		{
-			path: '/visitor/addVisitor',
-			name: 'addVisitor',
-			component: AddNewVisitor
-		},
-		{
-			path: '/personnel',
-			name: 'personnel',
-			component: PersonnelRecord
-		},
-		{
-			path: '/light/lightAdd',
-			name: 'lightAdd',
-			component: LightAdd
-		},
-		{
-			path: '/airConditioner',
-			name: 'airConditioner',
-			component: AirConditioner
-		},
-		{
-			path: '/device-fault',
-			name: 'device-fault',
-			component: DeviceFault
-		},
-		{
-			path: '/propertyManagement',
-			name: 'propertyManagement',
-			component: PropertyManagement
-		},
-		{
-			path: '/addProperty',
-			name: 'addProperty',
-			component: AddProperty
-		},
-		{
-			path: '/postManagement',
-			name: 'postManagement',
-			component: PostManagement
-		},
-		{
-			path: '/addPost',
-			name: 'addPost',
-			component: AddPost
-		},
-		{
-			path: '/energy-consumption',
-			nane: 'energy-consumption',
-			component: EnergyConsumption
-		},
-		{
-			path: '/modifyPsw',
-			name: 'modifyPsw',
-			component: ModifyPsw,
-			meta: {
-				login: true
-			}
-		},
 		{
 			path: '/login',
 			name: 'login',
@@ -165,27 +58,17 @@ const router = new Router({
 			}
 		},
 		{
-			path: '/dashboard',
-			name: 'dashboard',
-			component: Dashboard,
-			meta: {
-				dashboard: true
-			}
-		},
-
-		{
-			path: '/environment',
-			name: 'environment',
-			component: Environment
+			path: '*',
+			component: NotFound
 		}
 	]
 })
 
 router.beforeEach((to, from, next) => {
-	const uid = sessionStorage.getItem('uid')
+	const token = sessionStorage.getItem('token')
 
-	if (to.path.includes('/login')) {
-		if (uid) {
+	if (to.path === '/login') {
+		if (token) {
 			next('/')
 		} else {
 			next()
@@ -194,11 +77,171 @@ router.beforeEach((to, from, next) => {
 		return
 	}
 
-	if (uid) {
-		next()
+	if (token) {
+		if (to.path === '/login') {
+			next('/')
+		} else {
+			next()
+		}
 	} else {
-		next(`/login/?redirect=${encodeURIComponent(to.path)}`)
+		next('/login')
 	}
 })
+
+export const routesMap = [
+	{
+		type: 0,
+		path: '/',
+		name: 'overview',
+		component: Overview
+	},
+
+	{
+		type: 1,
+		path: '/energy-consumption',
+		nane: 'energy-consumption',
+		component: EnergyConsumption
+	},
+
+	{
+		type: 2,
+		path: '/environment',
+		name: 'environment',
+		component: Environment
+	},
+
+	{
+		type: 3,
+		path: '/propertyManagement',
+		name: 'propertyManagement',
+		component: PropertyManagement
+	},
+
+	{
+		type: 4,
+		path: '/postManagement',
+		name: 'postManagement',
+		component: PostManagement
+	},
+	{
+		type: 41,
+		path: '/addPost',
+		name: 'addPost',
+		component: AddPost
+	},
+
+	{
+		type: 5,
+		path: '/lease',
+		name: 'lease',
+		component: Lease
+	},
+	{
+		type: 51,
+		path: '/lease/add',
+		name: 'lease-add',
+		component: LeaseAdd
+	},
+	{
+		type: 52,
+		path: '/lease/detail/:id',
+		name: 'lease-detail',
+		component: LeaseDetail
+	},
+
+	{
+		type: 6,
+		path: '/parking-lot',
+		name: 'parking-lot',
+		component: ParkingLot
+	},
+	{
+		type: 61,
+		path: '/parking-lot/add',
+		name: 'parking-lot-add',
+		component: ParkingLotAdd
+	},
+	{
+		type: 62,
+		path: '/parking-lot/detail/:id',
+		name: 'parking-lot-detail',
+		component: ParkingLotDetail
+	},
+
+	{
+		type: 7,
+		path: '/proprietor',
+		name: 'proprietor',
+		component: Proprietor
+	},
+	{
+		type: 71,
+		path: '/addProperty',
+		name: 'addProperty',
+		component: AddProperty
+	},
+	{
+		type: 8,
+		path: '/visitor',
+		name: 'visitor',
+		component: Visitor
+	},
+	{
+		type: 81,
+		path: '/visitor/addVisitor',
+		name: 'addVisitor',
+		component: AddNewVisitor
+	},
+	{
+		type: 9,
+		path: '/personnel',
+		name: 'personnel',
+		component: PersonnelRecord
+	},
+
+	{
+		type: 10,
+		path: '/lightManagement',
+		name: 'lightManagement',
+		component: LightManagement
+	},
+	{
+		type: 101,
+		path: '/light/lightAdd',
+		name: 'lightAdd',
+		component: LightAdd
+	},
+	{
+		type: 11,
+		path: '/airConditioner',
+		name: 'airConditioner',
+		component: AirConditioner
+	},
+	{
+		type: 12,
+		path: '/device-fault',
+		name: 'device-fault',
+		component: DeviceFault
+	},
+
+	{
+		type: 13,
+		path: '/modifyPsw',
+		name: 'modifyPsw',
+		component: ModifyPsw,
+		meta: {
+			login: true
+		}
+	},
+	{
+		type: 14,
+		path: '/dashboard',
+		name: 'dashboard',
+		component: Dashboard,
+		meta: {
+			dashboard: true
+		}
+	}
+]
 
 export default router
