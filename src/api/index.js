ce400570397f494ff2eb69ef837ff3113340385b
hanyang <513915503@qq.com>
2018-11-15 19:21:51 +0800

    update: 添加公共安全配置

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,14 @@ import 'whatwg-fetch'
 import qs from 'qs'
 import Vue from 'vue'
 
+import store from '@/store'
+
+import Hashes from 'jshashes'
 import axios from 'axios'
+
+const SHA1 = new Hashes.SHA1
+const accessKeyId = 'APId1482c4fc9'
+const accessKeySecret = 'f96de898642941ee8401bce20a076f18'
 
 window.axios = axios
 
@@ -10,6 +17,17 @@ axios.defaults.timeout = 10000
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 
 axios.interceptors.request.use((config) => {
+	const timestamp = Date.now()
+	const action = config.data.action
+
+	// 安全相关
+	config.data.timestamp = timestamp
+	config.data.accessKeyId = accessKeyId
+	config.data.signature = SHA1.hex(`${accessKeySecret}_${accessKeyId}_${timestamp}_${action}`)
+
+	//  user token
+	config.headers['x-sam-Token'] = store.state.token
+
 	if (config.method === 'post') {
 		config.data = qs.stringify(config.data)
 	}
@@ -20,9 +38,9 @@ axios.interceptors.request.use((config) => {
 })
 
 axios.interceptors.response.use((res) => {
-	const {code, msg} = res.data
+	const {success, msg} = res.data
 
-	if (code !== 0) {
+	if (! success) {
 		Vue.prototype.$message.error(msg)
 
 		res.data = null
