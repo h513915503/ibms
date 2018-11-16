72c5c93d96554feaa57a73f65309b7544789ae0b
duanshilei <2233974230@qq.com>
2018-11-16 21:13:23 +0800

    add: 人员出入记录页面接口联调

--- src/views/personnelRecord/personnel.vue
+++ src/views/personnelRecord/personnel.vue
@@ -63,49 +63,55 @@
 
 <template>
     <div id="personnel-container">
-        <tab-bar :list="tabs"/>
+        <loading v-if="loading"></loading>
 
-        <div class="container">
-            <div class="operator">
-                <div class="search-content">
-					<label class="calendar-label">
-						<el-date-picker
-                            v-model="date"
-                            type="datetimerange"
-                            start-placeholder="开始时间"
-                            end-placeholder="结束时间">
-                        </el-date-picker>
-					</label>
-					<el-input placeholder="姓名/手机号码"></el-input>
-					<el-button type="primary" class="btn-search">查询</el-button>
-				</div>
-            </div>
+        <template v-else>
+            <tab-bar :list="tabs"/>
 
-            <div class="table-content">
-                <el-table :data="tableData">
-                    <el-table-column label="时间" prop="time" sortable></el-table-column>
-                    <el-table-column label="姓名" prop="name"></el-table-column>
-                    <el-table-column label="出/入" prop="outOrIn"></el-table-column>
-                    <el-table-column label="业主/访客" prop="role"></el-table-column>
-                    <el-table-column label="停留时长" prop="behind"></el-table-column>
-                    <el-table-column label="状态" prop="status" :filters="[]" :filter-method="filterHandler">
-                        <template slot-scope="scope">
-                            <span class="status error" v-if="scope.row.status === 0">异常</span>
-                            <span class="status normal" v-else-if="scope.row.status === 1">正常</span>
-                            <span class="status hangout" v-else-if="scope.row.status === 2">乱走</span>
-                            <span class="status left" v-else>已离开</span>
-                        </template>
-                    </el-table-column>
-                </el-table>
-                <div class="page-wrapper">
-                    <el-pagination
-                        background
-                        layout="prev, pager, next"
-                        :total="1000">
-                    </el-pagination>
+            <div class="container">
+                <div class="operator">
+                    <div class="search-content">
+                        <label class="calendar-label">
+                            <el-date-picker
+                                v-model="date"
+                                type="datetimerange"
+                                start-placeholder="开始时间"
+                                end-placeholder="结束时间">
+                            </el-date-picker>
+                        </label>
+                        <el-input placeholder="姓名/手机号码"></el-input>
+                        <el-button type="primary" class="btn-search">查询</el-button>
+                    </div>
+                </div>
+
+                <div class="table-content">
+                    <el-table :data="tableData">
+                        <el-table-column label="时间" prop="allDateTamp" sortable></el-table-column>
+                        <el-table-column label="姓名" prop="accountName"></el-table-column>
+                        <el-table-column label="出/入" prop="accessStatus"></el-table-column>
+                        <el-table-column label="业主/访客" prop="type"></el-table-column>
+                        <el-table-column label="停留时长" prop="stayTime"></el-table-column>
+                        <el-table-column label="状态" :filters="[]" :filter-method="filterHandler">
+                            <template slot-scope="scope">
+                                <span class="status error" v-if="scope.row.activeStatus === 5">异常</span>
+                                <span class="status normal" v-else-if="scope.row.activeStatus === 6">正常</span>
+                                <span class="status hangout" v-else-if="scope.row.activeStatus === 7">未进园区</span>
+                                <span class="status left" v-else>已离开</span>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                    <div class="page-wrapper">
+                        <el-pagination
+                            background
+                            layout="prev, pager, next"
+                            @current-change="pageChange"
+                            :total="totalItem">
+                        </el-pagination>
+                    </div>
                 </div>
             </div>
-        </div>
+        </template>
+        
     </div>
 </template>
 
@@ -115,64 +121,78 @@
     export default {
         data() {
             return {
+                loading: false,
                 date: '',
+                totalItem: '',
                 tabs: [
                     {
-                        number: 300,
+                        number: '',
                         text: '今日累计访客量'
                     },
                     {
-                        number: 200,
+                        number: '',
                         text: '当前园区访客'
                     },
                     {
-                        number: 100,
-                        text: '已离开园区访客'
+                        number: '',
+                        text: '当前开园区业主'
                     }
                 ],
-                tableData: [
-                    {
-                        time: '2012/2/3 4:32',
-                        name: 'duan shilei',
-                        outOrIn: '出',
-                        role: '业主',
-                        behind: '89',
-                        status: 0
-                    },
-                    {
-                        time: '2012/2/3 4:32',
-                        name: 'duan shilei',
-                        outOrIn: '出',
-                        role: '业主',
-                        behind: '89',
-                        status: 1
-                    },
-                    {
-                        time: '2012/2/3 4:32',
-                        name: 'duan shilei',
-                        outOrIn: '出',
-                        role: '业主',
-                        behind: '89',
-                        status: 2
-                    },
-                    {
-                        time: '2012/2/3 4:32',
-                        name: 'duan shilei',
-                        outOrIn: '出',
-                        role: '业主',
-                        behind: '89',
-                        status: 3
-                    },
-                ]
+                tableData: [],
+                pageNum: '1',
+                pageSize: '10',
+                date1: '',
+                date2: '',
+                userNameOrPhone: ''
             }
         },
         methods: {
             filterHandler() {
 
             },
+             pageChange(val) {
+                this.pageNum = val;
+                this.getTableData();
+            },
+            async getTopData() {
+                const params = {
+                    action: 'accountManagement.queryAllCount'
+                };
+                const data = await axios.post('/api/dispatcher.do', params);
+
+                if(! data) {
+                    return;
+                }
+
+                this.tabs[0].number = data.data.peopleCounting;
+                this.tabs[1].number = data.data.dqFkCount;
+                this.tabs[2].number = data.data.dqYZCount;
+            },
+            async getTableData() {
+                const params = {
+                    action: 'accountManagement.queryAccountManagementLogPage',
+                    pageNum: this.pageNum,
+                    pageSize: this.pageSize,
+                    fromTimeYZStr: this.date1,
+                    toTimeYZStr: this.date2,
+                    userNameOrPhone: this.userNameOrPhone
+                }
+
+                const data = await axios.post('/api/dispatcher.do', params);
+                if(!data) {
+                    return;
+                }
+
+                this.tableData = data.data.list;
+                this.totalItem = data.data.total;
+            }
         },
-        components: {
-            tabBar
+        created() {
+            this.loading = true;
+
+            Promise.all([this.getTopData(), this.getTableData()]).then(() => {
+                this.loading = false;
+            })
         }
     }
 </script>
