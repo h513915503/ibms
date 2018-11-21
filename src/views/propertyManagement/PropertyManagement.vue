221e724db3b9ab532ab304d58f384c3b3115b1fc
hanyang <513915503@qq.com>
2018-11-21 18:47:46 +0800

    update: 添加业务逻辑

--- src/views/propertyManagement/PropertyManagement.vue
+++ src/views/propertyManagement/PropertyManagement.vue
@@ -38,8 +38,8 @@
         margin-top: 20px;
         text-align: right;
     }
-    
-    
+
+
     .popover-btn-wrapper {
         text-align: right;
     }
@@ -53,63 +53,63 @@
         font-size: 12px;
         margin-right: 10px;
     }
+    .popover-wrapper {
+        width: 440px;
+    }
 </style>
 
 <template>
     <div id="property-wrapper">
-        <div class="no-data" v-if="index === 0">
-            <h1>暂无物业人员</h1>
-            <p>您还没有维护物业人员信息，</p>
-            <p class="bottom-tip">点击下方【+ 物业人员】按钮添加新的物业人员。</p>
-            <el-button type="primary" @click="go">+ 物业人员</el-button>
-        </div>
+        <loading v-if="loading"></loading>
 
-        <div class="container" v-else>
-            <div class="search-wrapper">
-				<el-button type="primary" @click="go">+ 物业人员</el-button>
-			</div>
+        <template v-else>
+            <div class="no-data" v-if="noData">
+                <h1>暂无物业人员</h1>
+                <p>您还没有维护物业人员信息，</p>
+                <p class="bottom-tip">点击下方【+ 物业人员】按钮添加新的物业人员。</p>
+                <el-button type="primary" @click="go">+ 物业人员</el-button>
+            </div>
 
-            <div class="table-content">
-                <el-table :data="tableData">
-                    <el-table-column prop="number" label="物业人员编号"></el-table-column>
-                    <el-table-column prop="name" label="姓名"></el-table-column>
-                    <el-table-column prop="phone" label="手机号码"></el-table-column>
-                    <el-table-column prop="post" label="岗位"></el-table-column>
-                    <el-table-column label="状态" :filters="[]" :filter-method="filterHandler">
-                        <template slot-scope="scope">
-                            <span class="job-status dimission" v-if="scope.row.status === 0">离职</span>
-                            <span class="job-status" v-else>在职</span>
-                        </template>
-                    </el-table-column>
-                    <el-table-column label="入职时间" sortable>
-                    </el-table-column>
-                    <el-table-column label="离职时间" sortable>
-                        <template slot-scope="scope">
-                            <span v-text="scope.row.time" v-if="scope.row.time"></span>
-                            <span v-else>暂未离职</span>
-                        </template>
-                    </el-table-column>
-                    <el-table-column label="操作">
-                        <template slot-scope="scope">
-                            <el-button class="opt-btn" type="text" size="small" @click="handleClick(scope.row)">编辑</el-button>
-                            <el-popover placement="bottom-end" title="该员工确定离职了么？" width="400" trigger="click" content="该员工确定离职了么？">
-                                <el-button class="opt-btn" type="text dimission" size="small" slot="reference" data-type="1">离职</el-button>
+            <div class="container" v-else>
+                <div class="search-wrapper">
+                    <el-button type="primary" @click="go">+ 物业人员</el-button>
+                </div>
 
-                                <p class="popover-content">将该员工设为离职之后，该员工的账号将被注销。</p>
-                                <div class="popover-btn-wrapper">
-                                    <el-button size="small">取消</el-button>
-                                    <el-button size="small" class="btn-confirm">确定</el-button>
-                                </div>
-                            </el-popover>
-                    </template>
-                    </el-table-column>
-                </el-table>
+                <div class="table-content">
+                    <el-table :data="list">
+                        <el-table-column prop="accountId" label="物业人员编号"></el-table-column>
+                        <el-table-column prop="accountName" label="姓名"></el-table-column>
+                        <el-table-column prop="phoneNumber" label="手机号码"></el-table-column>
+                        <el-table-column label="岗位">
+                            <template slot-scope="scope">
+                                <ul>
+                                    <li v-for="item of scope.row.postNameArray">
+                                        {{item}}
+                                    </li>
+                                </ul>
+                            </template>
+                        </el-table-column>
+                        <el-table-column prop="status" label="状态"></el-table-column>
+                        <el-table-column label="入职时间" prop="hireDate" sortable></el-table-column>
+                        <el-table-column label="操作">
+                            <template slot-scope="scope">
+                                <el-button class="opt-btn" type="text" size="small" @click="goEdit(scope.row)">编辑</el-button>
+                                <el-button class="opt-btn dimission" type="text" size="small" @click="forLeaveOffice($event, scope.row, scope.$index)">离职</el-button>
+                            </template>
+                        </el-table-column>
+                    </el-table>
 
-                <div class="page-wrapper">
-                    <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+                    <div class="page-wrapper">
+                        <el-pagination background layout="prev, pager, next" :total="pageTotal" @current-change="pageChange"></el-pagination>
+                    </div>
                 </div>
             </div>
-        </div>
+        </template>
+
+        <popover name="close" title="该员工确定离职了么？" content="将该员工设为离职之后，该员工的账号将被注销。" :popoverModalStatus.sync="popoverModalStatus" ref="popover">
+            <el-button slot="ok" @click="popoverModalStatus = false">取消</el-button>
+            <el-button type="primary" slot="cancel" class="ok" @click="leaveOffice">确定</el-button>
+        </popover>
 	</div>
 </template>
 
@@ -118,32 +118,98 @@
     export default {
         data() {
             return {
-                index: 1,
-                tableData: []
+                loading: false,
+                loaded: false,
+
+                page: 1,
+                pageTotal: 1,
+                list: [],
+
+                popoverModalStatus: false
             }
         },
-        created() {
-            this.getPropertyDataList()
+
+        computed: {
+            noData() {
+                return this.loaded && ! this.list.length
+            }
         },
+
+        created() {
+            this.loading = true
+
+            this.getList().then(() => {
+                this.loaded = true
+                this.loading = false
+            })
+        },
+
         methods: {
             go() {
-                this.$router.push('/addProperty')
+                this.$router.push('/propertyManagement/add')
             },
-            async getPropertyDataList() {
+            async getList() {
                 const params = {
-                    role: 0
-                };
-                const data = await axios.post('/getPropertyManagementList', params);
-                this.tableData = data.data;
+                    action: 'administrator.getPmoInfo',
+                    pageNo: this.page,
+                    pageSize: 10
+                }
+
+                const data = await axios.post('/api/dispatcher.do', params)
+
+                if (! data) {
+                    return
+                }
+
+                this.pageTotal = + data.data.total
+                this.list = data.data.accountInfoResList
             },
-            handleClick(row) {
-                // console.log(row)
-                this.$router.push(`/addProperty/${row.status}`);
-                this.$store.state.detailInfo = row
+            pageChange(value) {
+                this.page = value
+                this.getList()
+            },
+            goEdit(item) {
+                this.$tempData = item
+
+                this.$router.push(`/propertyManagement/edit/${item.accountId}`)
+            },
+            forLeaveOffice(e, item, index) {
+                this.$item = item
+                this.$index = index
+                this.popoverModalStatus = true
+
+                this.$nextTick(() => {
+                    const {x, y} = e.target.getBoundingClientRect()
+
+                    this.$refs.popover.$el.style.left = `${x - 750}px`
+                    this.$refs.popover.$el.style.top = `${y - 75}px`
+                })
+            },
+            async leaveOffice() {
+                const params = {
+                    action: 'administrator.dimissionOpreate',
+                    pmoId: this.$item.accountId,
+                    name: this.$item.accountName
+                }
+
+                // 不等请求返回，直接关闭弹窗，提高用户体验走一波
+                this.popoverModalStatus = false
+                const temp = this.list.splice(this.$index, 1)
+
+                const data = await axios.post('/api/dispatcher.do', params)
+
+                if (! data) {
+                    this.list.splice(this.$index, 0, temp[0])
+
+                    return
+                }
+
+                this.list.splice(this.$index, 1)
+
+                // 释放内存
+                this.$index = null
+                this.$item = null
             }
-        },
-        components: {
-            
         }
     }
 </script>
