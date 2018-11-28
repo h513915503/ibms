39f2356fab72aeb56585415d72c615539b66d26d
hanyang <513915503@qq.com>
2018-11-28 18:47:28 +0800

    opt: 1. 优化组件展示位置 2. 更新 ui

--- src/components/popover.vue
+++ src/components/popover.vue
@@ -1,42 +1,50 @@
 <style scoped>
 .popover-wrapper {
 	padding: 24px;
-	position: absolute;
+	position: fixed;
 	z-index: 10;
 	font-size: 16px;
 	border-radius: 4px;
 	box-shadow: 0 0px 12px rgba(0, 0, 0, .2);
 	background-color: #FFF;
-	transition: opacity .15s, transform .15s;
+	transition: all .15s;
 	transform-origin: 100% 0;
 }
-.scale-enter, .scale-leave-active {
+.scale-enter , .scale-leave-active  {
 	opacity: 0;
 	transform: scale(.7);
 }
-h4 {
+.mask {
+	width: 100%;
+	height: 100%;
+	position: fixed;
+	top: 0;
+	left: 0;
+	z-index: -1;
+}
+.title {
 	display: flex;
 	align-items: center;
-
-	& svg {
-		width: 22px;
-		height: 22px;
-		padding: 4px;
-		margin-right: 16px;
-		box-sizing: border-box;
-		border-radius: 50%;
-	}
+	margin-bottom: 16px;
 }
-.close svg {
+.icon {
+	width: 22px;
+	height: 22px;
+	padding: 4px;
+	margin-right: 16px;
+	box-sizing: border-box;
+	border-radius: 50%;
+}
+.close .icon {
 	background-color: #F5222D;
 }
-.question svg {
+.question .icon {
 	background-color: #FAAD14;
 }
-p {
+.content {
 	width: 335px;
 	padding-left: 40px;
-	margin: 12px 0 24px;
+	margin-bottom: 24px;
 	color: rgba(0, 0, 0, .65);
 	line-height: 1.6;
 }
@@ -44,60 +52,112 @@ p {
 	display: flex;
 	justify-content: flex-end;
 }
-/* .el-button {
-	width: 65px;
-	padding: 0;
-	line-height: 32px;
-}
-.el-button:last-child {
-	color: #F5222D;
-	border-color: #D9D9D9;
-	background-color: rgba(0, 0, 0, .04);
-} */
 </style>
 
 <template>
 	<transition name="scale">
-		<div class="popover-wrapper" ref="popover">
-			<h4 class="close" v-if="name === 'close'">
-				<svg>
-					<use xlink:href="#close"></use>
-				</svg>
-				{{title}}
-			</h4>
-			<h4 class="question" v-if="name === 'question'">
-				<svg>
-					<use xlink:href="#question"></use>
-				</svg>
-				{{title}}
-			</h4>
+			<div class="popover-wrapper" ref="popover">
+				<div class="mask"></div>
 
-			<p v-text="content"></p>
-			<div class="btn-wrapper">
-				<slot name="ok"></slot>
-				<slot name="cancel"></slot>
+				<h4 class="title close" v-if="name === 'close'">
+					<svg class="icon">
+						<use xlink:href="#close"></use>
+					</svg>
+					{{title}}
+				</h4>
+				<h4 class="title question" v-if="name === 'question'">
+					<svg class="icon">
+						<use xlink:href="#question"></use>
+					</svg>
+					{{title}}
+				</h4>
+
+				<p class="content" v-text="content"></p>
+
+				<div class="btn-wrapper">
+					<slot name="ok"></slot>
+					<slot name="cancel"></slot>
+				</div>
 			</div>
-		</div>
 	</transition>
 </template>
 
 <script>
 	export default {
-		props: ['name', 'title', 'content', 'popoverModalStatus'],
+		props: {
+			name: {
+				type: String,
+				default: 'close'
+			},
+			title: {
+				type: String,
+				required: true
+			},
+			content: {
+				type: String,
+				required: true
+			},
+			followTarget: {
+				type: HTMLElement,
+				required: true
+			}
+		},
 
 		mounted() {
+			const {x, y, bottom} = this.followTarget.getBoundingClientRect()
+			const popover = this.$refs.popover
+			const width = popover.offsetWidth
+			const height = popover.offsetHeight
+
+			// 更新 position
+			popover.style.left = `${x - width - 20}px`
+			popover.style.top = `${y + 20}px`
+
+			// 40 是间距
+			if (bottom + height + 40 > innerHeight) {
+				popover.style.transformOrigin = '100% 100%'
+				popover.style.top = `${y - height}px`
+			}
+
+			// 捕获事件
 			document.addEventListener('click', (e) => {
 				if (! this.$refs.popover) {
 					return
 				}
 
-				if (this.$refs.popover.contains(e.target)) {
+				// 点击自身不屏蔽，除了 .mask
+				if (this.$refs.popover.contains(e.target) && e.target.className !== 'mask') {
 					return
 				}
 
 				this.$emit('hide')
-				this.$emit('update:popoverModalStatus', false)
 			}, true)
+
+			// 自动更新 position
+			this.$resizewheelHandler = (e) => {
+				const {x, y, bottom} = this.followTarget.getBoundingClientRect()
+
+				popover.style.transformOrigin = '100% 0'
+				popover.style.left = `${x - width - 20}px`
+				popover.style.top = `${y + 20}px`
+
+				if (bottom + height + 40 > innerHeight) {
+					popover.style.transformOrigin = '100% 100%'
+					popover.style.top = `${y - height}px`
+				}
+			}
+
+			window.addEventListener('resize', this.$resizewheelHandler)
+
+			// 屏蔽滚动事件
+			this.$mousewheelHandler = (e) => e.preventDefault()
+
+			document.addEventListener('mousewheel', this.$mousewheelHandler)
+		},
+
+		destroyed() {
+			window.removeEventListener('resize', this.$resizewheelHandler)
+			document.removeEventListener('mousewheel', this.$mousewheelHandler)
 		}
 	}
 </script>
