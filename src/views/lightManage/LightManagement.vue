954aa63d096bb0e80f95805935d2b55452ad115c
duanshilei <2233974230@qq.com>
2018-11-30 20:33:25 +0800

    add:灯管理页面增加内容

--- src/views/lightManage/LightManagement.vue
+++ src/views/lightManage/LightManagement.vue
@@ -44,7 +44,20 @@
         padding: 24px;
     }
     .top-button {
+        display: flex;
+	    justify-content: space-between;
         padding-bottom: 24px;
+    }
+    .search-content {
+        display: flex;
+	    justify-content: space-between;
+    }
+    .light-search {
+        width: 128px;
+	    margin-left: 24px;
+    }
+    .btn-search {
+        margin-left: 24px;
     }
     .all-floor {
         /* width: 74px; */
@@ -83,6 +96,38 @@
     .detail-floor {
 
     }
+    .detail-info {
+        width: 260px;
+        margin-left: 24px;
+    }
+    .title {
+        margin-bottom: 24px;
+        font-size:14px;
+        color:rgba(0,0,0,1);
+    }
+    .about {
+        font-size:12px;
+        color:rgba(140,140,140,1);
+        line-height:24px;
+    }
+    .all-dvice {
+        margin-bottom: 24px;
+        font-size:14px;
+        line-height: 50px;
+        color:rgba(0,0,0,1);
+        border-bottom: 1px solid rgba(232, 232, 232, 1);
+        & span {
+            float: right;
+        }
+    }
+    .normal-li {
+        font-size: 12px;
+        line-height: 50px;
+        color: rgba(140,140,140,1);
+        & span {
+            float: right;
+        }
+    }
 </style>
 
 <template>
@@ -97,8 +142,17 @@
 
         <div class="deviceContent" v-if="currentIndex === 0">
             <div class="top-button">
-                <el-button type="primary">+ 灯</el-button>
-                <el-button>批量导入</el-button>
+                <div class="left-content">
+                    <el-button type="primary" @click="addDevice">+ 灯</el-button>
+                    <el-button>批量导入</el-button>
+                </div>
+                
+
+                <div class="search-content">
+                    <el-input class="light-search" placeholder="编号/品牌/型号"></el-input>
+                    <el-button type="primary" class="btn-search">查询</el-button>
+                </div>
+                
             </div>
             
             <div class="container">
@@ -107,6 +161,23 @@
                 </ul>
                 <div class="detail-floor">
                     <Building />
+                </div>
+
+                <div class="detail-info" v-if="level === 0">
+                    <p class="title">暂无设备</p>
+                    <span class="about">
+                        您可通过以下两种方式添加。<br /> 
+                        1. 单个添加：点击【+ 灯】按钮后，在楼层平面图中点击确定灯的位置，之后输入设备的各个信息，点击确定即可。 2. 批量导入：点击【批量导入】，系统会自动导入设备及其信息。
+                    </span>
+                </div>
+
+                <div class="have-device detail-info" v-if="level === 1">
+                    <ul class="divece-info">
+                        <li class="all-dvice">设备总数（台）<span>1234</span></li>
+                        <li class="normal-li">运行中（台）<span>1234</span></li>
+                        <li class="normal-li">故障中（台）<span>1234</span></li>
+                        <li class="normal-li">未运行（台）<span>1234</span></li>
+                    </ul>
                 </div>
             </div>
             
@@ -124,6 +195,7 @@
             return {
                 search: '',
                 currentIndex: 0,
+                level: 0,
                 floorIndex: 0,
                 floor: ['1F', '2F', '3F', '4F', '5F', '6F', '7F', '8F', '9F', '10F', '11F', '12F', '13F'],
 				tab: ['设备维护', '报表分析'],
@@ -188,6 +260,9 @@
             },
             switchFloor(index) {
                 this.floorIndex = index
+            },
+            addDevice() {
+                this.level = 1
             },
             filterHandler(value, row, column) {
 
