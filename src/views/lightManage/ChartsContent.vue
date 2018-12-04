1f7f3a6c623e1b3ff36d952a1f9f52b6ad853f9c
duanshilei <2233974230@qq.com>
2018-12-04 19:34:24 +0800

    add:新增柱状图内容

--- src/views/lightManage/ChartsContent.vue
+++ src/views/lightManage/ChartsContent.vue
@@ -41,12 +41,32 @@
     }
     .export {
         display: flex;
+        margin-right: 24px;
+        /* margin: 0 24px 0 auto; */
+    }
+    .option {
+        display: flex;
+        justify-content: space-between;
         margin: 0 24px 0 auto;
+        line-height: 40px;
+    }
+    .btn-text {
+        display: inline-block;
+        margin-right: 24px;
+        color: rgba(0, 0, 0, .65);
+        font-size: 14px;
+        cursor: pointer;
+    }
+    .actived {
+        color: #1890FF;
     }
     .content {
         display: flex;
         justify-content: space-between;
         padding-bottom: 76px;
+    }
+    .car-port {
+        width: 100%
     }
 </style>
 
@@ -67,21 +87,37 @@
                     <LightPieCard />
                     <LightPieCard />
                 </div>
-                
             </div>
             
+            <div class="car-port" v-if="areaIndex === 1">
+                <div class="header">
+                    <p class="title">灯设备分析</p>
+                    <div class="option">
+                        <div class="btn-text" :class="{actived: dateType === 0}" @click="dateType = 0">今日</div>
+                        <div class="btn-text" :class="{actived: dateType === 1}" @click="dateType = 1">7天内</div>
+                        <div class="btn-text" :class="{actived: dateType === 2}" @click="dateType = 2">30天内</div>
+                    </div>
+                    <el-button class="export">导出</el-button>
+                </div>
+                <div class="content">
+                    <LightBarChart />
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import mixin from '@/mixins'
     import LightPieCard from '@/components/light-management/pieChart.vue'
+    import LightBarChart from '@/components/light-management/barChart.vue'
 
     export default {
         data() {
             return {
                 areaIndex: 0,
-                areas: ['园区', '地下车库', '地上楼层', '走廊', '室外']
+                areas: ['园区', '地下车库', '地上楼层', '走廊', '室外'],
+                dateType: 0
             }
         },
         methods: {
@@ -89,8 +125,10 @@
                 this.areaIndex = index;
             }
         },
+        mixins: [mixin],
         components: {
-            LightPieCard
+            LightPieCard,
+            LightBarChart
         }
     }
 </script>
