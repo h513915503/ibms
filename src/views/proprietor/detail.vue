5a4a1017604e6ca7e2545f718f36227a183c812b
duanshilei <2233974230@qq.com>
2018-12-04 19:36:38 +0800

    add:业主管理查看详情页面的完善

--- src/views/proprietor/detail.vue
+++ src/views/proprietor/detail.vue
@@ -1,5 +1,5 @@
 <style scoped>
-#proprietor-add-wrapper {
+#proprietor-detail-wrapper {
 	padding: 24px;
 }
 .container {
@@ -8,103 +8,28 @@
 	box-sizing: border-box;
 	background-color: #FFF;
 }
-.el-input, .el-select {
-	width: 354px;
+.detail {
+	margin-top: 24px;
 }
-.file-wrapper {
-	display: flex;
-	align-items: center;
-	margin-top: 10px;
-	color: rgba(0, 0, 0, .45);
-	font-size: 14px;
-}
-.input-wrapper {
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	align-items: center;
-	width: 104px;
-	height: 104px;
-	padding: 15px 0;
-	margin-right: 10px;
-	cursor: pointer;
-	border-radius: 4px;
-	box-sizing: border-box;
-	border: 2px dashed rgba(0, 0, 0, .15);;
-	background-color: rgba(0, 0, 0, .02);
-}
-.icon {
-	width: 24px;
-	height: 24px;
-	position: relative;
-
-	&::before, &::after {
-		content: "";
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		background-color: rgba(0, 0, 0, .45);
-		transform: translate(-50%, -50%);
-	}
-
-	&::before {
-		width: 100%;
-		height: 2px;
-	}
-
-	&::after {
-		width: 2px;
-		height: 100%;
-	}
-}
-.upload {
-	display: none;
-}
-.face-image-wrapper {
-	display: flex;
-	align-items: center;
-	position: relative;
-	overflow: hidden;
-}
-.face-image {
-	width: 104px;
-	height: 104px;
-	margin-right: 12px;
-	object-fit: cover;
-}
-.scan-icon {
-	display: none;
-	width: 104px;
-	height: 104px;
-	position: absolute;
-	top: -100%;
-	left: 0;
-}
-.animation {
-	display: block;
-	animation: t 2s infinite;
-}
-@keyframes t {
-	from {
-		transform: translateY(0);
-	}
-	to {
-		transform: translateY(100%);
-	}
-}
-.recollection {
-	display: block;
-	color: #0E7CC2;
-	font-size: 14px;
-	cursor: pointer;
-}
-.bottom-tips {
+.item {
+	margin-bottom: 40px;
+	color: #919191;
 	font-size: 16px;
+
+	& span {
+		display: inline-block;
+		width: 200px;
+		margin-right: 15px;
+		text-align: right;
+	}
+}
+strong {
+	color: #000;
 }
 </style>
 
 <template>
-	<div id="proprietor-add-wrapper">
+	<div id="proprietor-detail-wrapper">
 		<el-breadcrumb separator="/">
 			<el-breadcrumb-item>人员</el-breadcrumb-item>
 			<el-breadcrumb-item :to="{path: '/proprietor'}">业主管理</el-breadcrumb-item>
@@ -113,6 +38,29 @@
 
 		<div class="container">
             <el-button @click="cancel">返回</el-button>
+
+			<div class="detail">
+				<div class="item">
+					<span>序号:</span>
+					<strong v-text="form.id"></strong>
+				</div>
+				<div class="item">
+					<span>业主姓名:</span>
+					<strong v-text="form.name"></strong>
+				</div>
+				<div class="item">
+					<span>业主手机号码:</span>
+					<strong v-text="form.phone"></strong>
+				</div>
+				<div class="item">
+					<span>所在单位:</span>
+					<strong v-text="form.company"></strong>
+				</div>
+				<div class="item">
+					<span>人脸信息:</span>
+					<img class="face-image" :src="faceURL">
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -128,6 +76,7 @@
 					company: '',
 					rentalCompanyId: ''
 				},
+				faceURL: ''
 			}
 		},
 		created() {
@@ -139,8 +88,6 @@
 			} else {
 				this.getDetail()
 			}
-
-			this.getCompanyList()
 		},
 
 		methods: {
