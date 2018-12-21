0a0dca2b50c46f1236136e7cd8076c352f2f4c16
hanyang <513915503@qq.com>
2018-12-21 18:16:15 +0800

    update: 页面加载完就挂载应用

--- src/main.js
+++ src/main.js
@@ -66,15 +66,8 @@ requireAllSvg(req)
 
 process.env.NODE_ENV === 'development' && (window.log = console.log.bind(console))
 
-// 登录后才需要异步挂载应用
-store.state.token ? store.dispatch('getUserInfo').then(() => {
-	new Vue({
-		router,
-		store,
-		render: h => h(App)
-	}).$mount('#app')
-}) : new Vue({
-		router,
-		store,
-		render: h => h(App)
-	}).$mount('#app')
+new Vue({
+	router,
+	store,
+	render: h => h(App)
+}).$mount('#app')
